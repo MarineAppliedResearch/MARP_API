bc6bf4cf72f03e22095e61d3587471379a8af27d
Isaac Assegai <isaac.a.travers@gmail.com>
2022-12-06 10:30:43 -0800

    We are now able to add an item to the fish db this partually addresses issue #7
    Signed-off-by: Isaac Assegai <isaac.a.travers@gmail.com>

--- repository/observation.repository.js
+++ repository/observation.repository.js
@@ -1,5 +1,6 @@
 const { connect } = require('../config/db.config');
 const logger = require('../logger/api.logger');
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 
 class ObservationRepository {
@@ -25,7 +26,7 @@ class ObservationRepository {
             return [];
         }
     }
-    
+
 
     async getObservationsBySessionID(session_id) {
         try {
@@ -42,10 +43,49 @@ class ObservationRepository {
         }
     }
 
+    async getMaxObservationIDInSession(session_id) {
+        try {
+            const observations = await this.db.observations.findAll({
+                where: {
+                    session_id: session_id
+                },
+                attributes: [Sequelize.fn('max', Sequelize.col('observation_id'))],
+            });
+            console.log('observations:::', observations);
+            return observations;
+        } catch (err) {
+            console.log(err);
+            return [];
+        }
+    }
+
     async createObservation(observation) {
         let data = {};
+        let max_obs = {};
+        let maxOBSID = -1;
+
         try {
+            max_obs = await this.db.observations.findAll({
+                where: {
+                    session_id: observation.session_id
+                },
+                attributes: [Sequelize.fn('max', Sequelize.col('observation_id'))],
+                raw: true,
+            }).then(function(observation_id){
+                maxOBSID = observation_id[0].max;
+             });
+            console.log('observations:::', max_obs);
+            
+        } catch (err) {
+            console.log(err);
+        }
+
+
+        try {
+             //first we need to get the max observation in the db.
+
             observation.createdate = new Date().toISOString();
+            observation.observation_id = maxOBSID + 1;
             data = await this.db.observations.create(observation);
         } catch(err) {
             logger.error('Error::' + err);
